3aa37908d0048f1523129c0427f16772cb784a91
Vipin Nair <swvist@gmail.com>
2013-02-05 13:02:18 +0530

    Adding waiting_list and stop functions

--- yaws/game_event_manager.erl
+++ yaws/game_event_manager.erl
@@ -1,7 +1,7 @@
 -module(game_event_manager).
 -define(GEM, ?MODULE).
 
--export([start/0, add_handler/1]).
+-export([start/0, add_handler/1, waiting_list/0]).
 
 start() ->
 	gen_event:start_link({local, ?GEM}).
@@ -9,4 +9,9 @@ start() ->
 add_handler({Mod, Options}) ->
 	gen_event:add_handler(?GEM, Mod, Options).
 
+waiting_list() ->
+	gen_event:call(?MODULE, game_event_handler, waiting_list).
+
+stop() ->
+	gen_event:stop(?GEM).
 
