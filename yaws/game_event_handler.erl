bee4c7feb90398aac0de695cd1e200412fd2afab
Vipin Nair <swvist@gmail.com>
2013-02-05 13:03:01 +0530

    Storing game states in ets

--- yaws/game_event_handler.erl
+++ yaws/game_event_handler.erl
@@ -5,15 +5,25 @@
 
 -export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3,terminate/2]).
 
+
 init([]) ->
-	{ok, []}.
+	{ok, ets:new(game_sessions, [set, public])}.
+
+handle_event({new_game, PID}, State) ->
+	ets:insert(State, {PID, waiting}),
+	{ok, State};
+
+handle_event({add_player, PID}, State) ->
+	ets:insert(State, {PID, playing}),
+	{ok, State};
 
 handle_event(Event, State) ->
 	io:format("Event ~p~n",[Event]),
 	{ok, State}.
 
-handle_call(_, State) ->
-	{ok, ok, State}.
+handle_call(waiting_list, State) ->
+	W = [ Game || {Game, _S} <- ets:match_object(State, {'_', waiting})],
+	{ok, W, State}.
 
 handle_info(_, State) ->
 	{ok, State}.
